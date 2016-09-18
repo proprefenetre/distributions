43cb158211a4f1a5dafc4f5404cd3494763d8b44
proprefenetre <proprefenetre@gmail.com>
2016-09-18 21:43:10 +0200

    add normalize(), P()

--- distribution.py
+++ distribution.py
@@ -6,12 +6,28 @@ from collections import Counter
 
 class Distribution(Counter):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._normalized = {}
+        self._updated = False
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self._updated = True
+
+    def update(self, *args, **kwargs):
+        super().update(*args, **kwargs)
+
     def normalized(self):
-        total = sum(self.values())
-        normalized = {}
-        for k, v in self.items():
-            normalized[k] = v / total
-        return normalized
+        if (not self._normalized) or self._updated:
+            total = sum(self.values())
+            for k, v in self.items():
+                self._normalized[k] = v / total
+        self._updated = False
+        return self._normalized
+
+    def P(self, key):
+        return self.normalized().get(key, 0)
 
     def __repr__(self):
         name = self.__class__.__name__
@@ -22,12 +38,8 @@ class Distribution(Counter):
 if __name__ == "__main__":
 
     x = Distribution({'vanilla': 20, 'chocolate': 20})
-    bowl = [('vanilla', 10), ('chocolate', 20)]
-    y = Distribution()
-    for k, v in bowl:
-        y[k] = v
-
-    print(y.normalized())
+    bowl = ['vanilla'] * 10 + ['chocolate'] * 20
+    y = Distribution(bowl)
     y.update(x)
-    print(y)
-    print(y.normalized())
+    for k in y:
+        print(k, y[k], y.P(k))

609441070af062be3c88116b2662be27b2c10c2a
proprefenetre <proprefenetre@gmail.com>
2016-09-27 17:58:31 +0200

    Monty Hall problem demonstration

--- distribution/distribution.py
+++ distribution/distribution.py
@@ -199,34 +199,66 @@ class DistGroup(Distribution):
                     raise ValueError('arguments should be Distributions')
         super().__init__(*args, **kwargs)
 
-    # def __getitem__(self, key):
-    #     for k, v in self._d.items():
-    #         if key == k.name:
-    #             return v
-
     def normalizer(self, item):
         joined = reduce(operator.add, self._d.keys())
         return joined.P(item)
 
-    def P(self, item):
-        if isinstance(item, str):
-            for k in self._d.keys():
-                if hasattr(k, 'name'):
-                    if k.name == item:
-                        return self._d[k] / self.total()
+    def P(self, key):
+        for k in self._d.keys():
+            if hasattr(k, 'name'):
+                if k.name == key:
+                    return self._d[k] / self.total()
         else:
-            return self._d.get(item, 0) / self.total()
+            raise KeyError('distribution {} not in distributions'.format(key))
 
-    def Ps(self, xs):
-        return [self.P(x) for x in xs]
+    def set(self, key, value):
+        for k in self._d.keys():
+            if hasattr(k, 'name'):
+                if k.name == key:
+                    self._d[k] = value
+        else:
+            raise KeyError('distribution {} not in distributions'.format(key))
 
 if __name__ == "__main__":
-    # cookies!
-    cookies = DistGroup([Distribution(['vanilla'] * 30 + ['chocolate'] * 10, name='bowl_1'),
-                         Distribution(['vanilla'] * 20 + ['chocolate'] * 20, name='bowl_2')])
-    data = 'vanilla'
-    cookies = cookies.normalize()
-    print(cookies)
-    print(cookies.normalizer(data))
-    print(cookies.P('bowl_1'))
-    print(cookies.total())
+
+    # Monty hall problem
+
+    # you have chosen door A. Do you switch to win a car?
+
+    # D: 
+    # Monty opens door B _and_ there is no car there
+    
+    # H:
+    # 1: the car is behind door A:
+    #       p(open A): 0    (we picked A)
+    #       p(open B): .5
+    #       p(open C): .5
+    hypo_1 = Distribution(name='1')
+    hypo_1.update({'A': 0, 'B': 0.5, 'C': 0.5})
+
+    # 2: the car is behind door B:
+    #       p(open A): 0    
+    #       p(open B): 0
+    #       p(open C): 1
+    hypo_2 = Distribution(name='2')
+    hypo_2.update({'A': 0, 'B': 0, 'C': 1})
+
+    # 3: the car is behind door C:
+    #       p(open A): 0    
+    #       p(open B): 1
+    #       p(open C): 0
+    hypo_3 = Distribution(name='3')
+    hypo_3.update({'A': 0, 'B': 1, 'C': 0})
+
+    # Priors
+    doors = DistGroup([hypo_1, hypo_2, hypo_3])
+
+    print('{:^5} {:<12} {:<12} {:<12} {:<12}'.format(' ', 'P(H)', 'P(D|H)', 'P(H)*P(D|H)', 'P(H|D)'))
+    for d in doors:
+        prior = doors.P(d.name)
+        likelihood = d.P('B')
+        print('{:^5} {:<12.5f} {:<12.5f} {:<12.5f} {:<12.5f}'.format(d.name,
+                                                                     prior,
+                                                                     likelihood,
+                                                                     prior * likelihood,
+                                                                     prior * likelihood / doors.normalizer('B')))
